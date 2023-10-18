045f3f568b8560cfc5a75849dc759b69490c30d0
Celeste <87475491+Celestialcece@users.noreply.github.com>
2023-10-18 14:22:30 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://celestialcece.github.io',
+  base: '/WeConnectPlus-Front',
+})
